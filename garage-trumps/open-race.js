ac5641a94e5ff105cd7f4393a93dfa831c76e94c
chrispearsontesting-spec <chris.pearson.testing@gmail.com>
2026-09-06 22:45:25 +0100

    Deep-link to jobs/DIY as well as the drag strip

--- garage-trumps/open-race.js
+++ garage-trumps/open-race.js
@@ -1,6 +1,11 @@
 setTimeout(function(){
-  if(/open=race/.test(location.search)){
+  var q=location.search||"";
+  if(/open=race/.test(q)){
     var b=document.getElementById("btnRace");
     if(b) b.click();
+  }
+  if(/open=jobs/.test(q)){
+    var j=document.getElementById("btnJobsDiy");
+    if(j) j.click();
   }
 },400);
